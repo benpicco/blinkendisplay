c0532a427e5fef2a57c341df25091c2070488aa5
Benjamin Valentin <benpicco@googlemail.com>
2018-11-24 15:56:40 +0100

    initial test

--- main.ino
+++ main.ino
@@ -1,48 +1,38 @@
-#include "FastLED.h"
+#include <FastLED.h>
+#include <LEDMatrix.h>
 
 // fast led constants
 #define DATA_PIN    6        // change to your data pin
 #define COLOR_ORDER GRB      // if colors are mismatched; change this
-#define NUM_LEDS    (75*8)       // change to the number of LEDs in your strip
+
+#define MATRIX_WIDTH   75
+#define MATRIX_HEIGHT  8
+#define MATRIX_TYPE    HORIZONTAL_ZIGZAG_MATRIX
+#define MATRIX_SIZE    (MATRIX_WIDTH*MATRIX_HEIGHT)
+#define NUM_LEDS       MATRIX_SIZE
 
 // change WS2812B to match your type of LED, if different
 // list of supported types is here:
 // https://github.com/FastLED/FastLED/wiki/Overview
 #define LED_TYPE    WS2812B
 
-// this creates an LED array to hold the values for each led in your strip
-CRGB leds[NUM_LEDS];
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
 
 void setup()
 {
 	// the wiki features a much more basic setup line:
-	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds[0], leds.Size());
+	FastLED.setCorrection(TypicalLEDStrip);
+	FastLED.setBrightness(5);
+	FastLED.clear(true);
 }
 
 void loop() 
 {
-
-  for(int dot = 0; dot < NUM_LEDS; dot++)
-  { 
-    
-    leds[dot] = CRGB::Blue;
-    FastLED.show();
-    
-    // clear this led for the next time around the loop
-    leds[dot] = CRGB::Black;
-    
-    delay(30);
-  }
-
-  for(int dot = 0; dot < NUM_LEDS; dot++)
-  { 
-    
-    leds[NUM_LEDS - dot] = CRGB::Blue;
-    FastLED.show();
-    
-    // clear this led for the next time around the loop
-    leds[NUM_LEDS - dot] = CRGB::Black;
-    
-    delay(30);
-  }
+	for (int i = 0; i < leds.Height(); ++i) {
+		FastLED.clear();
+	        leds.DrawLine(0, i, leds.Width(), leds.Height() - i, CRGB::Blue);
+		FastLED.show();
+		delay(30);
+	}
 }
